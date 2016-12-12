28ccfb7297107ec0f3640cb406055e021344c638
gdm03 <gabriel_v_delmundo@dlsu.edu.ph>
2016-12-12 20:26:50 +0800

    Fix config

--- MIPS_MP/src/config/Config.java
+++ MIPS_MP/src/config/Config.java
@@ -7,9 +7,9 @@ import java.util.Set;
 public class Config {
 
 	public static final int MEMORY_SIZE = 2048;
-	public static final int CODE_START = 1000;
+	public static final int CODE_START = 0x1000;
 	public static final int CODE_END = 0x2FFF;
-	public static final int DATA_START = 3000;
+	public static final int DATA_START = 0x3000;
 	public static final int DATA_END = 0x4FFF;
 	
 	public static final Integer[] BRANCH_VALUES = {Opcode.BEQC, Opcode.J, Opcode.BC};
