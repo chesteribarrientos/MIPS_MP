80a286e22d8d3ab895533afd645b17671e9c247f
chesteribarrientos <retxhellboy@gmail.com>
2016-12-12 23:54:37 +0800

    Conflict Fix

--- MIPS_MP/src/config/Config.java
+++ MIPS_MP/src/config/Config.java
@@ -6,11 +6,7 @@ import java.util.Set;
 
 public class Config {
 
-<<<<<<< HEAD
 	public static final int MEMORY_SIZE = 16384;
-=======
-	public static final int MEMORY_SIZE = 8192;
->>>>>>> origin/master
 	public static final int CODE_START = 0x1000;
 	public static final int CODE_END = 0x2FFF;
 	public static final int DATA_START = 0x3000;
