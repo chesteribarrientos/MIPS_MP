73794e938afd5f048031154d37368cc99a635ab7
gdm03 <gabriel_v_delmundo@dlsu.edu.ph>
2016-12-12 12:20:45 +0800

    Working XOR

--- MIPS_MP/src/driver/TestPipeline.java
+++ MIPS_MP/src/driver/TestPipeline.java
@@ -21,21 +21,29 @@ public class TestPipeline {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Machine machine = new Machine();
-		String code = "DADDIU r1, r0, 0x8001";
+		//String code = "DADDIU r1, r0, 0x8001";
+		String code = "XOR r5, r1, r3";
 		int opcode = InstructionUtils.getInstructionEnum(code).getInstructionConverter().getOpcode(code);
 		machine.storeWordToMemory(Config.CODE_START, opcode);
 		
 		System.out.print("Opcode fetched: ");
 		Print.as32bitHex(machine.loadWordFromMemory(Config.CODE_START));
 		
+		machine.storeToGPR(1, 3);
+		machine.storeToGPR(3, 5);
 		machine.doIFCycle();
 		machine.doIDCycle();
 		machine.doExCycle();
 		machine.doMemCycle();
 		machine.doWBCycle();
 		
-		System.out.print("R" + OpcodeUtils.rt(opcode) + " = ");
-		Print.as64bitHex(machine.loadFromGPR(1));
+		// I-type
+		//System.out.print("R" + OpcodeUtils.rt(opcode) + " = ");
+		//Print.as64bitHex(machine.loadFromGPR(1));
+		
+		// R-type
+		System.out.print("R" + OpcodeUtils.rd(opcode) + " = ");
+		Print.as64bitHex(machine.loadFromGPR(OpcodeUtils.rd(opcode)));
 		System.out.println("done");
 	}
 
--- MIPS_MP/src/instruction_set/XOR.java
+++ MIPS_MP/src/instruction_set/XOR.java
@@ -2,14 +2,49 @@ package instruction_set;
 
 import config.Opcode;
 import interfaces.IConverter;
+import interfaces.IExecutor;
+import machine.EXMEM;
+import machine.Machine;
+import utils.OpcodeUtils;
+import utils.Print;
 
 /**
  * @author laurencefoz
  */
-public class XOR extends RType implements IConverter {
+public class XOR extends RType implements IConverter, IExecutor {
     @Override
     public int getOpcode(String statement) {
     	//if (super.getRegisterOps(statement) == Opcode.NOP) return 0x0;
     	return (0b000000 << 26) | super.getRegisterOps(statement) | 0b00000 | Opcode.XOR;
     }
+    
+    @Override
+    public void execute(int opcode, Machine machine) {
+    	EXMEM exmem = (EXMEM) machine.getPipeline().get("EX/MEM");
+		
+		int rs = OpcodeUtils.rs(opcode);
+		long rsValue = machine.loadFromGPR(rs);
+		
+		int rt = OpcodeUtils.rt(opcode);
+		long rtValue = machine.loadFromGPR(rt);
+		
+		long result = rsValue ^ rtValue;
+		
+		System.out.println(Long.toBinaryString(rsValue));
+		System.out.println(Long.toBinaryString(rtValue));
+		System.out.println("Result: " + result);
+		exmem.setALUOutput(result);
+		exmem.setCond(false);
+    }
+    
+    @Override
+    public void execute_memory(int opcode, Machine machine) {
+    	super.doMemoryCycle(opcode, machine);
+    }
+    
+    @Override
+	public void execute_writeback(int opcode, Machine machine) {
+    	super.doWriteBack(opcode, machine);
+    }
+	
 }
