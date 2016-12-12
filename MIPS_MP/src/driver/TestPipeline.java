5e520b6af760867c4d9e62cae7987bd50b8d3b06
gdm03 <gabriel_v_delmundo@dlsu.edu.ph>
2016-12-12 12:44:21 +0800

    Working DSUBU

--- MIPS_MP/src/driver/TestPipeline.java
+++ MIPS_MP/src/driver/TestPipeline.java
@@ -22,15 +22,23 @@ public class TestPipeline {
 		// TODO Auto-generated method stub
 		Machine machine = new Machine();
 		//String code = "DADDIU r1, r0, 0x8001";
-		String code = "XOR r5, r1, r3";
+		//String code = "XOR r5, r1, r3";
+		String code = "DSUBU r5, r1, r3";
 		int opcode = InstructionUtils.getInstructionEnum(code).getInstructionConverter().getOpcode(code);
 		machine.storeWordToMemory(Config.CODE_START, opcode);
 		
 		System.out.print("Opcode fetched: ");
 		Print.as32bitHex(machine.loadWordFromMemory(Config.CODE_START));
 		
+		// test XOR values
 		machine.storeToGPR(1, 3);
 		machine.storeToGPR(3, 5);
+		
+		// test DSUBU
+		// note: dsubu is not unsigned
+		machine.storeToGPR(1, 1);
+		machine.storeToGPR(3, -2);
+		
 		machine.doIFCycle();
 		machine.doIDCycle();
 		machine.doExCycle();
@@ -39,12 +47,15 @@ public class TestPipeline {
 		
 		// I-type
 		//System.out.print("R" + OpcodeUtils.rt(opcode) + " = ");
-		//Print.as64bitHex(machine.loadFromGPR(1));
+		//Print.as64bitHex(machine.loadFromGPR(OpcodeUtils.rt(opcode)));
 		
 		// R-type
+		// XOR Example
 		System.out.print("R" + OpcodeUtils.rd(opcode) + " = ");
 		Print.as64bitHex(machine.loadFromGPR(OpcodeUtils.rd(opcode)));
 		System.out.println("done");
+		
+		// DSUBU Example
 	}
 
 }
